9b8609f86210d55cd765a48244b948f559f5dddf
Maciek Kumorek <mkumorek@gmail.com>
2021-11-21 15:08:21 -0800

    Implement a basic file read and write

--- src/file.ts
+++ src/file.ts
@@ -1,9 +1,21 @@
+/**
+ * Represents a file in the in-memory file system.
+ * The contents of the file are stored as a Buffer (byte sequence, fixed-length) encoded in utf-8.
+ * 
+ * Currently it supports over-writing the file, but nothing special like appening to the file.
+ */
 export class File {
     readonly name: string;
     private _contents?: Buffer;
     
-    get contents() {
-        return this._contents;
+    /**
+     * Get the file contents as a string
+     */
+    get contents(): string {
+        if (!this._contents) {
+            return "";
+        }
+        return this._contents.toString('utf-8');
     }
 
     constructor(name: string) {
@@ -16,8 +28,8 @@ export class File {
      * @returns number of bytes written
      */
     public write(toWrite: string) : number {
-        this._contents = Buffer.alloc(toWrite.length, 0);
-        const written = this._contents.write(toWrite, toWrite.length);
-        return written;
+        Buffer.from
+        this._contents = Buffer.from(toWrite, 'utf-8');
+        return this._contents.length;
     }
 }
--- tests/file.spec.ts
+++ tests/file.spec.ts
@@ -4,5 +4,16 @@ describe("File", () => {
     test("can be created with a name", () => {
         const f1 = new File("Hello.txt");
         expect(f1.name).toBe("Hello.txt");
-    })
+    });
+
+    test("can write to a file", () => {
+        const f1 = new File("Hello.txt");
+        expect(f1.contents).toBe("");
+
+        f1.write("Hello world");
+        expect(f1.contents).toBe("Hello world");
+
+        f1.write("Hello world2");
+        expect(f1.contents).toBe("Hello world2");
+    });
 });
